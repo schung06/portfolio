9dbbb527063d7bc2464c141645652bfaa9c5cd75
Stephanie <stephaniec328122716@gmail.com>
2026-02-16 01:23:02 -0800

    fixed tailwind stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./app/App";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import App from "./app/App.tsx";
+import "./styles/index.css";
+createRoot(document.getElementById("root")!).render(<App />);

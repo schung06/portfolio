0110e0bf48192d98eabc303bf9a43c79a1c4ff67
Stephanie <stephaniec328122716@gmail.com>
2026-02-16 13:08:09 -0800

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import "./styles/index.css";
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/portfolio/">
+    <App />
+  </BrowserRouter>,
+);
+
